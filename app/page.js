1c1fd28840d7b0dba2fe353ff6a66a5704a43d90
saad96043 <saad.sayyed@oxcytech.com>
2023-12-21 17:29:05 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,158 @@
 import Image from 'next/image'
-
+import logo from '../public/logo.png'
+import sign from '../public/sign.png'
+import QR from '../public/QR.png'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div >
+      <button className='bg-blue-500 text-white p-2 relative left-[83%] top-3 rounded-lg'>Download</button>
+      <div className='border h-[1161px] mx-auto  border-black lg:w-[80%] w-[600px]  mt-[25px] mb-[500px] text-[1rem]'>
+        <p className='text-center border-b border-black text-blue-500 font-bold'>TAX INVOICE</p>
+        <div className='lg:flex lg:flex-row flex flex-col'>
+          <div>
+            <div className='border-b  border-black border-r w-[600px] p-3 flex'>
+              <Image src={logo} height={200} width={120}/>
+              <div>
+                <h2>Hindustan Uniliever</h2>
+                <p>GSTIN 27AAACT2727Q1ZW</p>
+                <p>64, Whitefield Main Rd, Palm Meadows,White Field,</p>
+                <p>Bengaluru, 29-KARNATAKA 560066</p>
+                <p>Mobile 9999999999</p>
+              </div>
+            </div>
+            <div className='border-b border-black border-r w-[600px] p-3'>
+              <p>Customer Details</p>
+              <p>Gaurav Gupta</p>
+              <p>Billing address</p>
+              <p>Marathahahali-Sarajpur Outer Ring Road,</p>
+              <p>Kadabeesanahali</p>
+              <p>Bengaluru 29-KARNATAKA 560067</p>
+              <p>Ph:9999999999</p>
+            </div>
+          </div>
+          <div>
+            <div className='flex h-[60px]'>
+              <div className='border-r border-b p-3 border-black lg:w-[200px] w-[300px]'>
+                <p>Invoice #</p>
+                <p>INV-11</p>
+              </div>
+              <div className='border-b border-black lg:w-[210px] p-3 w-[300px]'>
+                <p>Invoice Date:</p>
+                <p>28 Mar 2022</p>
+              </div>
+            </div>
+            <div className='flex h-[60px]'>
+              <div className='border-r border-b border-black lg:w-[200px] w-[300px] p-3'>
+                <p>Invoice #</p>
+                <p>INV-11</p>
+              </div>
+              <div className='border-b border-black lg:w-[210px] w-[300px] p-3 '>
+                <p>Invoice Date:</p>
+                <p>28 Mar 2022</p>
+              </div>
+
+            </div>
+            <div className='border-b border-black h-[218px] p-3'>
+
+              <p>Shipping address:</p>
+              <p>Marathahali Solapur Outer Ring Road.</p>
+              <p>Kadabeesanahali</p>
+              <p>Bengaluru 29-KARNATAKA 560067</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <table>
+            <tr className='border-b border-black'>
+              <th className='text-sm w-[65px] border-r border-black'>#</th>
+              <th className='text-sm w-[1600px] text-left border-r border-black'>Item</th>
+              <th className='text-sm  border-r border-black'>HSN/SAC</th>
+              <th className='text-sm  w-[100px] border-r border-black'>Qty</th>
+              <th className='text-sm  w-[120px] border-r border-black'>Rate/Item</th>
+              <th className='text-sm w-[150px] border-r border-black'>Per</th>
+              <th className='text-sm w-[500px] text-right border-r border-black'>Amount</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Colgate 200gm</td>
+              <td>33061020</td>
+              <td className='text-right'>16 Nos</td>
+              <td className='text-right'>76.27</td>
+              <td className='text-right font-bold'>Nos</td>
+              <td className='text-right'>1,220.34</td>
+            </tr>
+            <tr className='h-[100px]'>
+              <td className=''>1</td>
+              <td>Surf Excel Easy Wash Detergent Powder 1kg
+              </td>
+              <td>340119</td>
+              <td className='text-right'>5 Nos</td>
+              <td className='text-right'>108.61 <br/>122.03(-11%)</td>
+              <td className='text-right font-bold'>Nos</td>
+              <td className='text-right'>543.05</td>
+            </tr>
+            
+            <tr className='h-[200px] border-b border-black'>
+              <td></td>
+              <td className='text-right font-bold'>Taxable Amount <br/> CGST 9.0% <br/> SGST 9.0% </td>
+              <td></td>
+              <td className='text-right'></td>
+              <td className='text-right'></td>
+              <td className='text-right'></td>
+              <td className='text-right font-bold'>1,763.35 <br/> 158.71 <br/> 158.71</td>
+            </tr>
+
+            <tr className='border-b border-black'>
+              <td></td>
+              <td className='text-right font-bold'>Total</td>
+              <td></td>
+              <td className='text-right font-bold'>21.00</td>
+              <td></td>
+              <td></td>
+              <td className='text-right font-bold'> 2,081.00</td>
+            </tr>
+
+          </table>
+          
+        </div>
+        <div className='border-b border-black'>
+          <p>Amount Chargable (in words) INR Two Thousand and eighty one rupees only</p>
+        </div>
+        <p className='text-right border-b border-black'>Amount Payable 2,081.00</p>
+        <div className='border-b border-black h-[190px] flex'>
+          <div className='flex justify-evenly  border-r border-black w-[65%] h-[190px]'>
+            <div className='p-3'>
+              <div><p>Bank Details:</p>
+                <p>Bank:  <span className='font-bold ml-3'>Yes Bank</span></p>
+                <p>Account#: <span className='font-bold ml-3'>999999999999</span></p>
+                <p>IFSC:<span className='font-bold ml-3'>YES9999</span></p>
+                <p>Branch: <span className='font-bold ml-3'>Kodhihali</span></p></div>
+            </div>
+            <div>
+              <p>Pay using UPI:</p>
+              <Image src={QR} width={120}/>
+            </div>
+          </div>
+          <div className='p-3 flex flex-col items-center'>
+            <p className='text-center text-[0.9rem]'> For Hindustan Uniliver</p>
+            <Image src={sign} width={120}/>
+          </div>
+        </div>
+        <div className='flex p-3'>
+          <div className='border-r border-black w-[50%]'>
+            <p>Notes:</p>
+            <p>Thank you for the Business</p>
+          </div>
+          <div className='p-3'>
+            <p className='font-bold'>Terms and Conditions:</p>
+            <p>1.Goods once sold cannot be taken</p>
+            <p>2.We are not the manufactureres , company <br /> will stand for warranty as per their terms</p>
+            <p>3.Interest @24% p.a. will be Charged for undeclared bills</p>
+            <p>4.Subject to local Juridication</p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
